a52a1bf5d46df64f7c48227d7e31a0e2c68b0dd5
Danylo Ivanchyshyn <50670248+DanyloIvanchyshyn@users.noreply.github.com>
2019-06-14 12:56:27 +0300

    v1.0.1
    Added:
    - route displaying
    - route points saving in the list feature
    - displaying points via clicking on the map
    - etc

--- Map_v1.0_project_team7_sa/Map_v1.0_project_team7_sa/Form1.Designer.cs
+++ Map_v1.0_project_team7_sa/Map_v1.0_project_team7_sa/Form1.Designer.cs
@@ -34,19 +34,23 @@
             this.txtLat = new System.Windows.Forms.TextBox();
             this.txtLong = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnLoadIntoMap = new System.Windows.Forms.Button();
+            this.btnAddPoint = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnClearList = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
             // splitter1
             // 
             this.splitter1.Location = new System.Drawing.Point(0, 0);
             this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(633, 602);
+            this.splitter1.Size = new System.Drawing.Size(633, 873);
             this.splitter1.TabIndex = 0;
             this.splitter1.TabStop = false;
             // 
@@ -75,10 +79,10 @@
             this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.map.ShowTileGridLines = false;
-            this.map.Size = new System.Drawing.Size(663, 602);
+            this.map.Size = new System.Drawing.Size(663, 873);
             this.map.TabIndex = 1;
             this.map.Zoom = 0D;
-            this.map.OnMapZoomChanged += new GMap.NET.MapZoomChanged(this.map_OnMapZoomChanged);
+            this.map.MouseClick += new System.Windows.Forms.MouseEventHandler(this.map_MouseClick);
             // 
             // label1
             // 
@@ -101,7 +105,7 @@
             // txtLong
             // 
             this.txtLong.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtLong.Location = new System.Drawing.Point(715, 157);
+            this.txtLong.Location = new System.Drawing.Point(715, 135);
             this.txtLong.Name = "txtLong";
             this.txtLong.Size = new System.Drawing.Size(139, 22);
             this.txtLong.TabIndex = 5;
@@ -110,79 +114,123 @@
             // 
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(757, 122);
+            this.label2.Location = new System.Drawing.Point(757, 100);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(62, 17);
             this.label2.TabIndex = 4;
             this.label2.Text = "Довгота";
             // 
-            // textBox1
+            // btnLoadIntoMap
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(715, 462);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(139, 22);
-            this.textBox1.TabIndex = 10;
+            this.btnLoadIntoMap.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLoadIntoMap.Location = new System.Drawing.Point(703, 191);
+            this.btnLoadIntoMap.Name = "btnLoadIntoMap";
+            this.btnLoadIntoMap.Size = new System.Drawing.Size(179, 27);
+            this.btnLoadIntoMap.TabIndex = 12;
+            this.btnLoadIntoMap.Text = "Нанести на карту";
+            this.btnLoadIntoMap.UseVisualStyleBackColor = true;
+            this.btnLoadIntoMap.Click += new System.EventHandler(this.btnLoadIntoMap_Click);
+            // 
+            // btnAddPoint
+            // 
+            this.btnAddPoint.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddPoint.Location = new System.Drawing.Point(692, 238);
+            this.btnAddPoint.Name = "btnAddPoint";
+            this.btnAddPoint.Size = new System.Drawing.Size(197, 32);
+            this.btnAddPoint.TabIndex = 13;
+            this.btnAddPoint.Text = "Зберегти точку";
+            this.btnAddPoint.UseVisualStyleBackColor = true;
+            this.btnAddPoint.Click += new System.EventHandler(this.btnAddPoint_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(692, 293);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 34);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Побудувати маршрут";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnGetRoute_Click);
+            // 
+            // btnClearList
+            // 
+            this.btnClearList.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClearList.Location = new System.Drawing.Point(703, 401);
+            this.btnClearList.Name = "btnClearList";
+            this.btnClearList.Size = new System.Drawing.Size(179, 27);
+            this.btnClearList.TabIndex = 15;
+            this.btnClearList.Text = "Очистити список точок";
+            this.btnClearList.UseVisualStyleBackColor = true;
+            this.btnClearList.Click += new System.EventHandler(this.btnClearList_Click);
             // 
             // label3
             // 
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(757, 427);
+            this.label3.Location = new System.Drawing.Point(669, 489);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 17);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "Довгота";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox2.Location = new System.Drawing.Point(715, 380);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(139, 22);
-            this.textBox2.TabIndex = 8;
+            this.label3.Size = new System.Drawing.Size(193, 18);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Довжина маршруту:";
             // 
             // label4
             // 
             this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(757, 346);
+            this.label4.Location = new System.Drawing.Point(836, 519);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(58, 17);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Широта";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 16;
             // 
-            // button2
+            // button5
             // 
-            this.button2.Location = new System.Drawing.Point(700, 214);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(179, 28);
-            this.button2.TabIndex = 12;
-            this.button2.Text = "Нанести на карту";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(669, 568);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(237, 222);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // label5
             // 
-            this.button3.Location = new System.Drawing.Point(669, 265);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(233, 43);
-            this.button3.TabIndex = 13;
-            this.button3.Text = "Нанести на карту з активного курсора";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(669, 519);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 17);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Адреса:";
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRemove.Location = new System.Drawing.Point(718, 347);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(144, 38);
+            this.btnRemove.TabIndex = 19;
+            this.btnRemove.Text = "Очистити карту";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(914, 602);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
+            this.ClientSize = new System.Drawing.Size(914, 873);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnClearList);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAddPoint);
+            this.Controls.Add(this.btnLoadIntoMap);
             this.Controls.Add(this.txtLong);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtLat);
@@ -191,6 +239,7 @@
             this.Controls.Add(this.splitter1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -204,12 +253,16 @@
         private System.Windows.Forms.TextBox txtLat;
         private System.Windows.Forms.TextBox txtLong;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnLoadIntoMap;
+        private System.Windows.Forms.Button btnAddPoint;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnClearList;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- Map_v1.0_project_team7_sa/Map_v1.0_project_team7_sa/Form1.cs
+++ Map_v1.0_project_team7_sa/Map_v1.0_project_team7_sa/Form1.cs
@@ -1,32 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
 using System.Windows.Forms;
 using GMap.NET;
 using GMap.NET.MapProviders;
 using GMap.NET.WindowsForms;
 using GMap.NET.WindowsForms.Markers;
 
+
 namespace Map_v1._0_project_team7_sa
 {
     public partial class Form1 : Form
     {
         private List<PointLatLng> _points;
+
         public Form1()
         {
             InitializeComponent();
             _points = new List<PointLatLng>();
-            map.DragButton = MouseButtons.Left;
-            map.MapProvider = GMapProviders.GoogleMap;
-            map.MinZoom = 5;
-            map.MaxZoom = 18;
-            map.Zoom = 10;
-            //map.Refresh();
-        }              
 
-        private void button2_Click(object sender, EventArgs e)
+            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://www.mapquestapi.com/directions/v2/route?key=KEY&from=Denver%2C+CO&to=Boulder%2C+CO&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false");
+            //string authInfo = "hrystynavladyka18:ms63hYgsGEq62dnX";
+            //authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
+            //request.Headers.Add("Authorization", "Basic " + authInfo);
+            //request.Credentials = new NetworkCredential("hrystynavladyka18", "ms63hYgsGEq62dnX");
+            //request.Method = WebRequestMethods.Http.Get;
+            //request.AllowAutoRedirect = true;
+            //request.Proxy = null;
+            //HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            //Stream stream = response.GetResponseStream();
+            //StreamReader streamreader = new StreamReader(stream);
+            //string s = streamreader.ReadToEnd();
+            //button5.Text = s;
+
+
+            //OkHttpClient client = new OkHttpClient();
+
+            //Request request = new Request.Builder()
+            //        .url("https://graphhopper.com/api/1/route?point=51.131,12.414&point=48.224,3.867&vehicle=car&locale=de&calc_points=false&key=api_key")
+            //        .get()
+            //        .build();
+
+            //Response response = client.newCall(request).execute();
+        }
+
+        private void btnLoadIntoMap_Click(object sender, EventArgs e)
         {
             var point = new PointLatLng(Convert.ToDouble(txtLat.Text), Convert.ToDouble(txtLong.Text));
-            //LoadMap(point);
+            LoadMap(point);
             AddMarker(point);
         }
 
@@ -35,7 +59,7 @@ namespace Map_v1._0_project_team7_sa
             map.Position = point;
         }
 
-        private void AddMarker(PointLatLng pointToAdd, GMarkerGoogleType markerType = GMarkerGoogleType.red_pushpin)
+        private void AddMarker(PointLatLng pointToAdd, GMarkerGoogleType markerType = GMarkerGoogleType.arrow)
         {
             var markers = new GMapOverlay("markers");
             var marker = new GMarkerGoogle(pointToAdd, markerType);
@@ -43,22 +67,104 @@ namespace Map_v1._0_project_team7_sa
             map.Overlays.Add(markers);
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void btnAddPoint_Click(object sender, EventArgs e)
         {
             _points.Add(new PointLatLng(Convert.ToDouble(txtLat.Text),
                 Convert.ToDouble(txtLong.Text)));
         }
 
-        private void map_AutoSizeChanged(object sender, EventArgs e)
+        private void btnGetRoute_Click(object sender, EventArgs e)
         {
-            var point = new PointLatLng(Convert.ToDouble(txtLat.Text), Convert.ToDouble(txtLong.Text));
-            LoadMap(point);
+            var route = GoogleSatelliteMapProvider.Instance
+               .GetRoute(_points[0], _points[1], false, false, 14);
+            
+
+            var r = new GMapRoute(route.Points, "My Route")
+            {
+                Stroke = new Pen(Color.Red, 5)
+            };
+                   
+            var routes = new GMapOverlay("routes");
+            routes.Routes.Add(r);
+            map.Overlays.Add(routes);
+            
+            map.Refresh();
+            map.Update();
+            label4.Text = route.Distance + @" Km";
         }
 
-        private void map_OnMapZoomChanged()
+        private void Form1_Load(object sender, EventArgs e)
         {
-            var point = new PointLatLng(Convert.ToDouble(txtLat.Text), Convert.ToDouble(txtLong.Text));
-            LoadMap(point);
+            GMaps.Instance.Mode = AccessMode.ServerAndCache;
+            map.CacheLocation = @"AIzaSyB_ZBxpV6axQxnIyDSf23lyg54w_vprPOk";
+            map.DragButton = MouseButtons.Left;
+            //map.Update();
+            map.MapProvider = GMapProviders.GoogleMap;
+            map.ShowCenter = false;
+            map.SetPositionByKeywords("Lviv, Ukraine");
+            map.MaxZoom = 20;
+            map.MinZoom = 0;
+            map.ShowCenter = false;
+        }
+
+        private void btnClearList_Click(object sender, EventArgs e)
+        {
+            _points.Clear();
+            map.Update();
+        }
+
+        private List<String> GetAddress(PointLatLng point)
+        {
+            List<Placemark> placemarks = null;
+            var statusCode = GMapProviders.GoogleMap.GetPlacemarks(point, out placemarks);
+            if (statusCode == GeoCoderStatusCode.OK && placemarks != null)
+            {
+                List<String> addresses = new List<string>();
+                foreach (var placemark in placemarks)
+                {
+                    addresses.Add(placemark.Address);
+                }
+                return addresses;
+            }
+            return null;
+        }
+
+        private void map_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                var point = map.FromLocalToLatLng(e.X, e.Y);
+                double lat = point.Lat;
+                double lng = point.Lng;
+
+                txtLat.Text = lat + "";
+                txtLong.Text = lng + "";
+
+                // Load Location
+                LoadMap(point);
+
+                // Adding Marker
+                AddMarker(point);
+
+                // Get Address
+                var addresses = GetAddress(point);
+                map.Refresh();
+                // Display Address
+                if (addresses != null)
+                    label5.Text = "Address: \n-----------------------\n" + addresses[0];
+                else
+                    label5.Text = "Unable To Load Address";
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (map.Overlays.Count > 0)
+            {
+                map.Overlays.RemoveAt(0);
+                map.Refresh();
+            }
         }
     }
-}
+}    
+
