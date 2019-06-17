a6ef20f5bf908bebb7a4c90067c35869294cfc5a
Danylo Ivanchyshyn <50670248+DanyloIvanchyshyn@users.noreply.github.com>
2019-06-17 09:45:54 +0300

    v1.0.2
    - minor bug fixes
    - dispaying adress of the route
    - calculating the distance of the route
    - etc

--- Map_v1.0_project_team7_sa/Map_v1.0_project_team7_sa/Main.Designer.cs
+++ Map_v1.0_project_team7_sa/Map_v1.0_project_team7_sa/Main.Designer.cs
@@ -50,9 +50,10 @@
             // 
             this.btnMenu.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.btnMenu.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMenu.Location = new System.Drawing.Point(12, 12);
+            this.btnMenu.Location = new System.Drawing.Point(16, 15);
+            this.btnMenu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnMenu.Name = "btnMenu";
-            this.btnMenu.Size = new System.Drawing.Size(160, 42);
+            this.btnMenu.Size = new System.Drawing.Size(213, 52);
             this.btnMenu.TabIndex = 1;
             this.btnMenu.Text = "Меню маршрутів";
             this.btnMenu.UseVisualStyleBackColor = false;
@@ -62,9 +63,10 @@
             // 
             this.btnAdd.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.btnAdd.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdd.Location = new System.Drawing.Point(1068, 14);
+            this.btnAdd.Location = new System.Drawing.Point(1424, 17);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(160, 40);
+            this.btnAdd.Size = new System.Drawing.Size(213, 49);
             this.btnAdd.TabIndex = 2;
             this.btnAdd.Text = "Додати маршрут";
             this.btnAdd.UseVisualStyleBackColor = false;
@@ -83,18 +85,20 @@
             this.pnlRoutes.Controls.Add(this.btnShow);
             this.pnlRoutes.Controls.Add(this.btnEdit1);
             this.pnlRoutes.Controls.Add(this.btnDelete);
-            this.pnlRoutes.Location = new System.Drawing.Point(6, 68);
+            this.pnlRoutes.Location = new System.Drawing.Point(8, 84);
+            this.pnlRoutes.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlRoutes.Name = "pnlRoutes";
-            this.pnlRoutes.Size = new System.Drawing.Size(202, 572);
+            this.pnlRoutes.Size = new System.Drawing.Size(269, 704);
             this.pnlRoutes.TabIndex = 4;
             // 
             // btnHideAll
             // 
             this.btnHideAll.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.btnHideAll.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnHideAll.Location = new System.Drawing.Point(9, 414);
+            this.btnHideAll.Location = new System.Drawing.Point(12, 510);
+            this.btnHideAll.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnHideAll.Name = "btnHideAll";
-            this.btnHideAll.Size = new System.Drawing.Size(187, 24);
+            this.btnHideAll.Size = new System.Drawing.Size(249, 30);
             this.btnHideAll.TabIndex = 9;
             this.btnHideAll.Text = "Сховати всі маршрути";
             this.btnHideAll.UseVisualStyleBackColor = false;
@@ -103,27 +107,30 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 38);
+            this.label1.Location = new System.Drawing.Point(8, 47);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.Size = new System.Drawing.Size(80, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Маршрут 1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 232);
+            this.label2.Location = new System.Drawing.Point(8, 286);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.Size = new System.Drawing.Size(80, 17);
             this.label2.TabIndex = 5;
             this.label2.Text = "Маршрут 2";
             // 
             // button8
             // 
             this.button8.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button8.Location = new System.Drawing.Point(93, 350);
+            this.button8.Location = new System.Drawing.Point(124, 431);
+            this.button8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(103, 27);
+            this.button8.Size = new System.Drawing.Size(137, 33);
             this.button8.TabIndex = 8;
             this.button8.Text = "Сховати";
             this.button8.UseVisualStyleBackColor = false;
@@ -131,9 +138,10 @@
             // btnHide
             // 
             this.btnHide.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.btnHide.Location = new System.Drawing.Point(93, 149);
+            this.btnHide.Location = new System.Drawing.Point(124, 183);
+            this.btnHide.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnHide.Name = "btnHide";
-            this.btnHide.Size = new System.Drawing.Size(103, 27);
+            this.btnHide.Size = new System.Drawing.Size(137, 33);
             this.btnHide.TabIndex = 4;
             this.btnHide.Text = "Сховати";
             this.btnHide.UseVisualStyleBackColor = false;
@@ -141,9 +149,10 @@
             // button7
             // 
             this.button7.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button7.Location = new System.Drawing.Point(93, 298);
+            this.button7.Location = new System.Drawing.Point(124, 367);
+            this.button7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(103, 46);
+            this.button7.Size = new System.Drawing.Size(137, 57);
             this.button7.TabIndex = 7;
             this.button7.Text = "Показати на карті";
             this.button7.UseVisualStyleBackColor = false;
@@ -151,9 +160,10 @@
             // btnEdit
             // 
             this.btnEdit.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.btnEdit.Location = new System.Drawing.Point(93, 31);
+            this.btnEdit.Location = new System.Drawing.Point(124, 38);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(103, 27);
+            this.btnEdit.Size = new System.Drawing.Size(137, 33);
             this.btnEdit.TabIndex = 1;
             this.btnEdit.Text = "Редагувати";
             this.btnEdit.UseVisualStyleBackColor = false;
@@ -162,9 +172,10 @@
             // button6
             // 
             this.button6.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button6.Location = new System.Drawing.Point(93, 265);
+            this.button6.Location = new System.Drawing.Point(124, 326);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(103, 27);
+            this.button6.Size = new System.Drawing.Size(137, 33);
             this.button6.TabIndex = 6;
             this.button6.Text = "Видалити";
             this.button6.UseVisualStyleBackColor = false;
@@ -172,9 +183,10 @@
             // btnShow
             // 
             this.btnShow.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.btnShow.Location = new System.Drawing.Point(93, 97);
+            this.btnShow.Location = new System.Drawing.Point(124, 119);
+            this.btnShow.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnShow.Name = "btnShow";
-            this.btnShow.Size = new System.Drawing.Size(103, 46);
+            this.btnShow.Size = new System.Drawing.Size(137, 57);
             this.btnShow.TabIndex = 3;
             this.btnShow.Text = "Показати на карті";
             this.btnShow.UseVisualStyleBackColor = false;
@@ -182,9 +194,10 @@
             // btnEdit1
             // 
             this.btnEdit1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.btnEdit1.Location = new System.Drawing.Point(93, 232);
+            this.btnEdit1.Location = new System.Drawing.Point(124, 286);
+            this.btnEdit1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnEdit1.Name = "btnEdit1";
-            this.btnEdit1.Size = new System.Drawing.Size(103, 27);
+            this.btnEdit1.Size = new System.Drawing.Size(137, 33);
             this.btnEdit1.TabIndex = 3;
             this.btnEdit1.Text = "Редагувати";
             this.btnEdit1.UseVisualStyleBackColor = false;
@@ -192,31 +205,34 @@
             // btnDelete
             // 
             this.btnDelete.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.btnDelete.Location = new System.Drawing.Point(93, 64);
+            this.btnDelete.Location = new System.Drawing.Point(124, 79);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(103, 27);
+            this.btnDelete.Size = new System.Drawing.Size(137, 33);
             this.btnDelete.TabIndex = 2;
             this.btnDelete.Text = "Видалити";
             this.btnDelete.UseVisualStyleBackColor = false;
             // 
             // webBrowser1
             // 
-            this.webBrowser1.Location = new System.Drawing.Point(208, 12);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Location = new System.Drawing.Point(277, 15);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(27, 25);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(854, 656);
+            this.webBrowser1.Size = new System.Drawing.Size(1139, 807);
             this.webBrowser1.TabIndex = 10;
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LavenderBlush;
-            this.ClientSize = new System.Drawing.Size(1240, 680);
+            this.ClientSize = new System.Drawing.Size(1653, 837);
             this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.pnlRoutes);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.btnMenu);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Main";
             this.Text = "Main";
             this.pnlRoutes.ResumeLayout(false);
